45ace5a92cfce6ab794f40fd2f57512dd8cc49c3
Kost-prog <kostyuk.mila@gmail.com>
2025-04-24 23:21:47 +0300

    first build

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+import './js/header.js';
+import './js/api.js';
+import './js/form';
+import './js/projects';
+import './js/faq.js';
