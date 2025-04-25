8bf4411bc0829e51c3c836d46340a46e9bfe5259
Kost-prog <kostyuk.mila@gmail.com>
2025-04-25 15:54:15 +0300

    created repo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
 import axios from 'axios';
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
-
-import './js/header.js';
-import './js/api.js';
-import './js/form';
-import './js/projects';
-import './js/faq.js';
