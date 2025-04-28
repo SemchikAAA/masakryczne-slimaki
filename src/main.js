fc6d927153bb6e70de3ee51dfc1e44f435e490b3
Andrii S <semtrololo@gmail.com>
2025-04-29 01:58:19 +0200

    add all import in main.js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 import 'modern-normalize/modern-normalize.css';
 
 import './js/faq.js';
+import './js/header.js';
+import './js/projects.js';
+import './js/reviews-api.js';
+import './work-together';
