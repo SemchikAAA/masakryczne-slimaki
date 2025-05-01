c06f110c11ab65063300435b24ddbc3fceb4ca91
Andrii S <semtrololo@gmail.com>
2025-05-01 15:14:17 +0200

    projects img

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,53 +1,53 @@
 const projects = [
   {
-    image: '/src/img/projects/wallet-webservice.jpg',
-    img2x: '/src/img/projects/wallet-webservice@2x.jpg',
+    image: '/img/projects/wallet-webservice.jpg',
+    img2x: '/img/projects/wallet-webservice@2x.jpg',
     title: 'Wallet webservice',
   },
   {
-    image: '/src/img/projects/green-harvest.jpg',
-    img2x: '/src/img/projects/green-harvest@2x.jpg',
+    image: '/img/projects/green-harvest.jpg',
+    img2x: '/img/projects/green-harvest@2x.jpg',
     title: 'Green harvest webservice',
   },
   {
-    image: '/src/img/projects/english-excellence.jpg',
-    img2x: '/src/img/projects/english-excellence@2x.jpg',
+    image: '/img/projects/english-excellence.jpg',
+    img2x: '/img/projects/english-excellence@2x.jpg',
     title: 'English Exellence website',
   },
   {
-    image: '/src/img/projects/power-pulse-webservice.jpg',
-    img2x: '/src/img/projects/power-pulse-webservice@2x.jpg',
+    image: '/img/projects/power-pulse-webservice.jpg',
+    img2x: '/img/projects/power-pulse-webservice@2x.jpg',
     title: 'power pulse webservice',
   },
 
   {
-    image: '/src/img/projects/mimino-website.jpg',
-    img2x: '/src/img/projects/mimino-website@2x.jpg',
+    image: '/img/projects/mimino-website.jpg',
+    img2x: '/img/projects/mimino-website@2x.jpg',
     title: 'mimino website',
   },
   {
-    image: '/src/img/projects/vyshyvanka-vibes.jpg',
-    img2x: '/src/img/projects/vyshyvanka-vibes@2x.jpg',
+    image: '/img/projects/vyshyvanka-vibes.jpg',
+    img2x: '/img/projects/vyshyvanka-vibes@2x.jpg',
     title: 'vyshyvanka vibes Landing Page',
   },
   {
-    image: '/src/img/projects/chego-jewelry.jpg',
-    img2x: '/src/img/projects/chego-jewelry@2x.jpg',
+    image: '/img/projects/chego-jewelry.jpg',
+    img2x: '/img/projects/chego-jewelry@2x.jpg',
     title: 'chego jewelry website',
   },
   {
-    image: '/src/img/projects/energy-flow.jpg',
-    img2x: '/src/img/projects/energy-flow@2x.jpg',
+    image: '/img/projects/energy-flow.jpg',
+    img2x: '/img/projects/energy-flow@2x.jpg',
     title: 'energy flow webservice',
   },
   {
-    image: '/src/img/projects/fruitbox-online-store.jpg',
-    img2x: '/src/img/projects/fruitbox-online-store@2x.jpg',
+    image: '/img/projects/fruitbox-online-store.jpg',
+    img2x: '/img/projects/fruitbox-online-store@2x.jpg',
     title: 'fruitbox online store',
   },
   {
-    image: '/src/img/projects/starlight-studio.jpg',
-    img2x: '/src/img/projects/starlight-studio@2x.jpg',
+    image: '/img/projects/starlight-studio.jpg',
+    img2x: '/img/projects/starlight-studio@2x.jpg',
     title: 'starlight studio landing page',
   },
 ];
@@ -83,7 +83,7 @@ function getMarkup(projects) {
                     VISIT
                     <div class="visit-arrow">
                       <svg width="18" height="18">
-                        <use href="/src/img/icon/icons.svg#icon-visit-icon"></use>
+                        <use href="/img/icon/icons.svg#icon-visit-icon"></use>
                       </svg>
                     </div>
                     </a>
